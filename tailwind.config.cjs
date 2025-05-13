bbd328eb488ec83907fb14a8a20a10ec0ee40620
M R N GOPI KRISHNA <gopi.muktevi@gmail.com>
2025-05-13 21:54:55 +0530

    initial checkin

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0047AB',
+        accent: '#FF6F00',
+        text: '#1C1C1C',
+        bg: '#F5F5F5',
+      },
+    },
+  },
+  plugins: [],
+};
